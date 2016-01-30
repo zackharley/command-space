4fd94aba1617982ba890729718d8580e54a7f80e
valencra <rafv2172@gmail.com>
2016-01-30 18:20:43 -0500

    Added tabs

--- app/js/skipptime.js
+++ app/js/skipptime.js
@@ -1,5 +1,27 @@
 var app = angular.module('skippTime', []);
 
 app.controller('mainController', function($scope){
-
-})
+  $scope.activeTab = 'Video';
+  $scope.manualData = [
+    {
+      'Tags': ['Python', 'Raise', 'Exception'],
+      'Link': 'https://docs.python.org/2/tutorial/errors.html#raising-exceptions'
+    }
+  ];
+  $scope.forumData = [
+    {
+      'Tags': ['Python', 'Raise', 'Exception'],
+      'Link': 'http://stackoverflow.com/questions/13052991/proper-way-to-raise-exception-in-python'
+    }
+  ];
+  $scope.videoData = [
+    {
+      'Tags': ['Python', 'Raise', 'Exception'],
+      'Link': 'https://www.youtube.com/watch?v=AXREU-nells'
+    }
+  ];
+  $scope.setActiveTab = function(tab){
+    $scope.activeTab = tab;
+  };
+  $
+});
