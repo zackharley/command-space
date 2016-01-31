e488a4a127f571f656e1af26dd141407a5d23405
zharley18 <zackharley@gmail.com>
2016-01-30 21:07:19 -0500

    Make search bar generate YouTube links

--- app/js/skipptime.js
+++ app/js/skipptime.js
@@ -3,6 +3,8 @@ var youtubeApiKey = 'AIzaSyCMI8_O4dN8Q28q-_X1LLThi4-B1QMjxoU';
 var clientID = '89157465201-1m8t3v3lketegd6v5ivnr7e70kgeju5i.apps.googleusercontent.com';
 var clientSecret = 'e19SlC66IOATuDzZmeguigwH';
 
+var answerClass = '.accepted-answer';
+
 //gapi.client.setApiKey(youtubeApiKey);
 
 app.controller('mainController', function ($scope) {
@@ -32,20 +34,29 @@ app.controller('mainController', function ($scope) {
 });
 
 var youtubeLinks = [];
-$(document).ready(function () {
-    $.get(link, function (data) {
-        console.log(data);
-        $(".result").html(data);
-        for (var i in data.items) {
-            youtubeLinks[i] = 'https://www.youtube.com/watch?v=' + data.items[i].id.videoId;
-        }
-        alert("Load was performed");
-        console.log(youtubeLinks);
-    });
+$(document).keypress(function (e) {
+    if (e.which == 13) {
+        var query = processQuery($('#search-bar').prop('value'));
+        var link = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + query + "&relevanceLanguage=en&type=video&key=" + youtubeApiKey;
+        $.get(link, function (data) {
+            console.log(data);
+            $(".result").html(data);
+            for (var i in data.items) {
+                youtubeLinks[i] = 'https://www.youtube.com/watch?v=' + data.items[i].id.videoId;
+            }
+            alert("Load was performed");
+            console.log(youtubeLinks);
+        });
+    }
 });
 
+function processQuery(text) {
+    console.log(text);
+    return text;
+}
 
-var link = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=photoshop+merge+layers&relevanceLanguage=en&type=video&key=" + youtubeApiKey;
+
+/*var link = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=photoshop+merge+layers&relevanceLanguage=en&type=video&key=" + youtubeApiKey;*/
 
 // Search for a specified string.
 /*function search(link) {
